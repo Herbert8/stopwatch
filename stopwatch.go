7dee5f079e88324222f90eef81abde10b3305f1d
Gin <tecpoirot@foxmail.com>
2023-05-27 21:07:07 +0800

    feat: 完善 Checkpoint 功能。
    1、通过 Checkpoint 计算时间差；
    2、Checkpoint 提供指定标签功能，Checkpoint 增加详情。

--- stopwatch.go
+++ stopwatch.go
@@ -8,11 +8,15 @@ import (
 	"time"
 )
 
+type Checkpoint struct {
+	tag  string
+	time time.Time
+}
+
 type Stopwatch struct {
-	name                string
-	startTime           time.Time
-	lastCheckpointIndex int
-	lastCheckpoint      time.Time
+	name        string
+	startTime   time.Time
+	checkpoints []Checkpoint
 }
 
 // 日志
@@ -29,10 +33,14 @@ func init() {
 func NewStopwatch(name string) *Stopwatch {
 	now := time.Now()
 	return &Stopwatch{
-		name:                name,
-		startTime:           now,
-		lastCheckpointIndex: 0,
-		lastCheckpoint:      now,
+		name:      name,
+		startTime: now,
+		checkpoints: []Checkpoint{
+			{
+				tag:  "Origin",
+				time: now,
+			},
+		},
 	}
 }
 
@@ -52,31 +60,32 @@ func (receiver *Stopwatch) SinceStart() time.Duration {
 }
 
 // LastCheckpoint 上一次的检查点
-func (receiver *Stopwatch) LastCheckpoint() time.Time {
-	return receiver.lastCheckpoint
-}
-
-// LastCheckpointIndex 上一次的检查点序号
-func (receiver *Stopwatch) LastCheckpointIndex() int {
-	return receiver.lastCheckpointIndex
+func (receiver *Stopwatch) LastCheckpoint() Checkpoint {
+	return receiver.checkpoints[len(receiver.checkpoints)-1]
 }
 
 // SinceLastCheckpoint 距离上次检查点的时间间隔
-func (receiver *Stopwatch) SinceLastCheckpoint() time.Duration {
+func (receiver *Stopwatch) SinceLastCheckpoint(tag string) time.Duration {
 	now := time.Now()
-	duration := now.Sub(receiver.lastCheckpoint)
+	lastCheckpoint := receiver.LastCheckpoint()
+	duration := now.Sub(lastCheckpoint.time)
 	var mutex sync.Mutex
 	mutex.Lock()
-	receiver.lastCheckpointIndex++
-	receiver.lastCheckpoint = now
+	newCheckpoint := Checkpoint{
+		tag:  tag,
+		time: now,
+	}
+	receiver.checkpoints = append(receiver.checkpoints, newCheckpoint)
 	defer mutex.Unlock()
 	return duration
 }
 
 // PrintDurationSinceLastCheckpoint 显示距离上次检查点的时间间隔
-func (receiver *Stopwatch) PrintDurationSinceLastCheckpoint() {
-	duration := receiver.SinceLastCheckpoint()
-	s := fmt.Sprintf("[%s] [since checkpoint %d-%d]: %s", receiver.name, receiver.lastCheckpointIndex, receiver.lastCheckpointIndex-1, duration)
+func (receiver *Stopwatch) PrintDurationSinceLastCheckpoint(tag string) {
+	lastCheckpoint := receiver.LastCheckpoint()
+	duration := receiver.SinceLastCheckpoint(tag)
+	newCheckpoint := receiver.LastCheckpoint()
+	s := fmt.Sprintf("[%s] since [Checkpoint] [%s->%s]: %s", receiver.name, lastCheckpoint.tag, newCheckpoint.tag, duration)
 	printString(s)
 }
 
@@ -88,6 +97,6 @@ func printString(s string) {
 // PrintDurationSinceStart 显示距离开始时间的时间间隔
 func (receiver *Stopwatch) PrintDurationSinceStart() {
 	duration := receiver.SinceStart()
-	s := fmt.Sprintf("[%s] since start: %s", receiver.name, duration)
+	s := fmt.Sprintf("[%s] since [Start]: %s", receiver.name, duration)
 	printString(s)
 }
